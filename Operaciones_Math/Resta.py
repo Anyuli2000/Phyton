6d3c1beeff03beba31585fcb1585687440f1baf1
Anyuli2000 <89560022+Anyuli2000@users.noreply.github.com>
2021-08-26 21:44:36 -0500

    Create Resta.py

---
+++ Operaciones_Math/Resta.py
@@ -0,0 +1,7 @@
+#Dados dos números enteros A y B calcular la diferencia de los mismos.
+#Entrada: Los números A y B
+#Salida: El resultado de la operación A-B
+
+A = int (input())
+B = int (input())
+print(A-B)
